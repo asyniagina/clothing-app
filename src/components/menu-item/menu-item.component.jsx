04834ebab504edda7ed2617394fa1eefe50503a0
Alina <alina.sinyagina@gmail.com>
2021-09-28 10:37:31 -0700

    fix

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { withRouter } from 'react-router';
+import { withRouter } from 'react-router-dom';
 
 import './menu-item.styles.scss';
 
 const MenuItem = ({title, imageUrl, size, history, linkUrl, match}) => (
     <div 
         className={`${size} menu-item`}
-        onclick={() => history.push(`${match.url}${linkUrl}`)}
+        onClick={() => history.push(`${match.url}${linkUrl}`)}
     >
         <div
             className='background-image'
